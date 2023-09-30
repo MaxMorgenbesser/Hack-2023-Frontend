9da07fe9726f73ea5adac2f2d20a2735ccc877a8
JoshBenPhilip <joshben4c@gmail.com>
2023-09-30 18:50:12 -0400

    working carosel

--- src/pages/Auth/PinComponents/FunFactCarosel.tsx
+++ src/pages/Auth/PinComponents/FunFactCarosel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import PagerView from "react-native-pager-view";
 
@@ -9,8 +9,26 @@ const FunFactsCarousel: React.FC = () => {
     "Did you know? A day on Venus is longer than its year. It takes Venus about 243 Earth days to rotate once on its axis but it takes only 225 Earth days to orbit the Sun.",
   ]);
 
+  const [currentPage, setCurrentPage] = useState(0);
+  const pagerRef = useRef<PagerView>(null);
+
+  useEffect(() => {
+    const changePage = () => {
+      let nextPage = currentPage + 1;
+      if (nextPage >= funFacts.length) {
+        nextPage = 0;
+      }
+      setCurrentPage(nextPage);
+      pagerRef.current?.setPage(nextPage);
+    };
+
+    const interval = setInterval(changePage, 3000); // change page every 3 seconds
+
+    return () => clearInterval(interval); // Clear the interval when the component unmounts
+  }, [currentPage, funFacts.length]);
+
   return (
-    <PagerView style={styles.viewPager} initialPage={0}>
+    <PagerView ref={pagerRef} style={styles.viewPager} initialPage={0}>
       {funFacts.map((fact, index) => (
         <View key={index} style={styles.page}>
           <Text>{fact}</Text>
